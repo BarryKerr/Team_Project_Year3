bb3e998674ffddb6f6484562617257530a784cab
barry <barrykerr83@gmail.com>
2016-11-25 09:36:02 +0000

    first commit

---
+++ EstateAgent/frmAddEmployee.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace EstateAgent
+{
+    public partial class frmAddEmployee : Form
+    {
+        MySqlDataAdapter adapter;
+        DataTable dt = new DataTable();
+        MySqlCommand cmd;
+        private string conn;
+        private MySqlConnection connect;
+
+        public frmAddEmployee()
+        {
+            InitializeComponent();
+        }
+        private void db_connection()
+        {
+            try
+            {
+                conn = "Server=localhost;Database=estate;Uid=root;Pwd=;";
+                connect = new MySqlConnection(conn);
+                connect.Open();
+            }
+            catch (MySqlException e)
+            {
+                throw;
+            }
+        }
+        private void clearTextBox()
+        {
+            txtBoxFname.Text = "";
+            txtBoxSurname.Text = "";
+            txtBoxPassword.Text = "";
+            txtBoxSalary.Text = "";
+        }
+        private void frmAddEmployee_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void add(string fname, string sName, string password, string salary)
+        {
+            db_connection();
+            string sql = "Insert into employee(fName,sName,password,salary) Values(@fname, @sName, @password,@salary)";
+            MySqlCommand cmd = new MySqlCommand(sql, connect);
+            // add params
+            cmd.Parameters.AddWithValue("@fname", fname);
+            cmd.Parameters.AddWithValue("@sName", sName);
+            cmd.Parameters.AddWithValue("@password", password);
+            cmd.Parameters.AddWithValue("@salary", salary);
+
+            try
+            {
+                db_connection();
+                if (cmd.ExecuteNonQuery() > 0)
+                {
+                    clearTextBox();
+                    MessageBox.Show("Successfully Inserted!");
+                }
+                connect.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                connect.Close();
+            }
+
+
+        }
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if(txtBoxFname.Text=="" || txtBoxSurname.Text=="" || txtBoxPassword.Text=="" || txtBoxSalary.Text=="")
+            {
+                MessageBox.Show("Enter All Details");
+            }
+            else
+            {
+                if (txtBoxSalary.Text.All(char.IsDigit))
+                {
+                    if(!txtBoxFname.Text.Any(char.IsDigit) || !txtBoxSurname.Text.Any(char.IsDigit))
+                    {
+                        add(txtBoxFname.Text, txtBoxSurname.Text, txtBoxPassword.Text, txtBoxSalary.Text);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Enter correct Details!");
+                    }
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Enter Numeric Value for Salary!");
+                }
+            }
+                
+            
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            frmUpdateStaff formCalled = new frmUpdateStaff();
+            formCalled.Show();
+            this.Close();
+        }
+    }
+}
---
+++ EstateAgent/frmProperty.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace EstateAgent
+{
+    public partial class frmProperty : Form
+    {
+        private int pID;
+
+        public frmProperty()
+        {
+            InitializeComponent();
+
+        }
+
+        private void frmProperty_Load(object sender, EventArgs e)
+        {
+            Load_Table();
+            txtHouseNum.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            txtAddress.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+            txtTown.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+            txtPrice.Text = dataGridView1.SelectedRows[0].Cells[4].Value.ToString();
+            string selectedIndex = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            pID = Convert.ToInt32(selectedIndex);
+        }
+
+
+        private void Load_Table()
+        {
+            string con = "datasource=localhost;Database=estate;Uid=root;Pwd=;";
+            MySqlConnection conDataBase = new MySqlConnection(con);
+            MySqlCommand cmd = new MySqlCommand("select propertyID,HouseNo,Address,Town,Price,Status from estate.property order by Town;", conDataBase);
+
+
+            try
+            {
+
+                MySqlDataAdapter sda = new MySqlDataAdapter();
+                sda.SelectCommand = cmd;
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                BindingSource bSource = new BindingSource();
+
+                bSource.DataSource = dt;
+                dataGridView1.DataSource = bSource;
+                sda.Update(dt);
+
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+        private void btnRemove_Click_1(object sender, EventArgs e)
+        {
+            string constr = "datasource=localhost;Database=estate;Uid=root;Pwd=;";
+            string query = "delete from estate.property where propertyID = " + pID + ";";
+            MySqlConnection conDataBase = new MySqlConnection(constr);
+            MySqlCommand cmdDataBase = new MySqlCommand(query, conDataBase);
+            MySqlDataReader myReadr;
+            try
+            {
+                conDataBase.Open();
+                myReadr = cmdDataBase.ExecuteReader();
+                MessageBox.Show("Property has been deleted successfully");
+                while (myReadr.Read())
+                {
+                }
+                conDataBase.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            Load_Table();
+        }
+
+        private void btnAdd_Click_1(object sender, EventArgs e)
+        {
+            frmAddProperty frm = new frmAddProperty();
+            frm.Show();
+            this.Hide();
+        }
+        private void dataGridView1_MouseClick_1(object sender, MouseEventArgs e)
+        {
+            string selectedIndex = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            pID = Convert.ToInt32(selectedIndex);
+        }
+        private void btnExit_Click_1(object sender, EventArgs e)
+        {
+            frmMain frm = new frmMain();
+            frm.Show();
+            this.Close();
+        }
+        private void btnUpdate_Click_1(object sender, EventArgs e)
+        {
+            
+            if (!txtAddress.Text.Any(char.IsDigit) && !txtTown.Text.Any(char.IsDigit))
+            {
+                if (txtPrice.Text.All(char.IsDigit) && txtHouseNum.Text.All(char.IsDigit))
+                {
+
+                    if (txtAddress.Text == "" || txtPrice.Text == "" || txtTown.Text == "")
+                    {
+                        MessageBox.Show("Please Enter all details to update!");
+                    }
+                    else
+                    {
+                        Decimal price = Convert.ToDecimal(txtPrice.Text);
+
+                        string constr = "datasource=localhost;Database=estate;Uid=root;Pwd=;";
+                        string query = "UPDATE property set HouseNo = '" + txtHouseNum.Text + "',Address = '" + txtAddress.Text + "',Town='" + txtTown.Text + "',price='" + price + "' where propertyID =" + pID + "";
+
+                        MySqlConnection conDataBase = new MySqlConnection(constr);
+                        MySqlCommand cmdDataBase = new MySqlCommand(query, conDataBase);
+                        MySqlDataReader myReadr;
+                        try
+                        {
+                            conDataBase.Open();
+                            myReadr = cmdDataBase.ExecuteReader();
+                            MessageBox.Show("Property has been successfully updated.");
+                            while (myReadr.Read())
+                            {
+                            }
+                            conDataBase.Close();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Enter Valid value for Price and House Number!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Enter correct details!");
+            }
+                
+            //MessageBox.Show("Select a property from the list of properties you want to update");
+
+
+            
+            Load_Table();
+            txtHouseNum.Text = "";
+            txtAddress.Text = "";
+            txtTown.Text = "";
+            txtPrice.Text = "";
+            txtHouseNum.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            txtAddress.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+            txtTown.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+            txtPrice.Text = dataGridView1.SelectedRows[0].Cells[4].Value.ToString();
+
+        }
+        private void dataGridView1_Click_1(object sender, EventArgs e)
+        {
+            txtHouseNum.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            txtAddress.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+            txtTown.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+            txtPrice.Text = dataGridView1.SelectedRows[0].Cells[4].Value.ToString();
+        }
+
+        // Methods That dont work But cant remove ------------------------------------------------------------
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            frmAddProperty frm = new frmAddProperty();
+            frm.Show();
+            this.Hide();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            string constr = "datasource=localhost;Database=estate;Uid=root;Pwd=;";
+            string query = "delete from estate.property where propertyID = " + pID + ";";
+            MySqlConnection conDataBase = new MySqlConnection(constr);
+            MySqlCommand cmdDataBase = new MySqlCommand(query, conDataBase);
+            MySqlDataReader myReadr;
+            try
+            {
+                conDataBase.Open();
+                myReadr = cmdDataBase.ExecuteReader();
+                MessageBox.Show("Property has been deleted successfully");
+                while (myReadr.Read())
+                {
+                }
+                conDataBase.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            Load_Table();
+            txtHouseNum.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            txtAddress.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+            txtTown.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+            txtPrice.Text = dataGridView1.SelectedRows[0].Cells[4].Value.ToString();
+        }
+
+        private void dataGridView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            string selectedIndex = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            pID = Convert.ToInt32(selectedIndex);
+        }
+       
+//---------------------------------
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Select a property from the list of properties you want to update");
+
+            Decimal price = Convert.ToDecimal(txtPrice.Text);
+
+            string constr = "datasource=localhost;Database=estate;Uid=root;Pwd=;";
+            string query = "UPDATE property set HouseNo = '" + txtHouseNum.Text + "',Address = '" + txtAddress.Text + "',Town='" + txtTown.Text + "',price='" + price + "' where propertyID =" + pID + "";
+
+            MySqlConnection conDataBase = new MySqlConnection(constr);
+            MySqlCommand cmdDataBase = new MySqlCommand(query, conDataBase);
+            MySqlDataReader myReadr;
+            try
+            {
+                conDataBase.Open();
+                myReadr = cmdDataBase.ExecuteReader();
+                MessageBox.Show("Property has been successfully updated.");
+                while (myReadr.Read())
+                {
+                }
+                conDataBase.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            Load_Table();
+            txtHouseNum.Text = "";
+            txtAddress.Text = "";
+            txtTown.Text = "";
+            txtPrice.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            frmMain frm = new frmMain();
+            frm.Show();
+            this.Close();
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+            txtHouseNum.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            txtAddress.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+            txtTown.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+            txtPrice.Text = dataGridView1.SelectedRows[0].Cells[4].Value.ToString();
+        }
+
+        
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+       
+
+        
+
+       
+
+       
+
+       
+
+
+    }
+}
